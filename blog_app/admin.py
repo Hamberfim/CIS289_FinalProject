1a4bf6d6a031f19c3b41e7df656ee693216e22a4
Hamberfim <adhamlingithub@gmail.com>
2020-12-03 16:22:53 -0600

    Fresh Setup

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# import the class models from models.py and register them
+from .models import Topic
 
 # Register your models here.
+admin.site.register(Topic)
--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+# This model tells Django how to work with the data that will be stored in the app
+class Topic(models.Model):
+    """A topic the admin user enters.
+    This class inherits from the 'Model' parent class that's included in Django.
+    """
+    text = models.CharField(max_length=255)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
