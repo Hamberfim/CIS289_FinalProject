b23bbaf79f4117e73a722bd037d89f670a76cd92
Hamberfim <adhamlingithub@gmail.com>
2020-12-04 10:59:59 -0600

    started user registration

--- users_app/urls.py
+++ users_app/urls.py
@@ -1,11 +1,13 @@
-"""Defines the urls patterns for the users app"""
+"""Defines the urls patterns for the users control app"""
 from django.urls import path
 from django.urls import include
-
+from . import views
 
 app_name = 'users'
 urlpatterns = [
     # default authorization url for django's default login view
     path('', include('django.contrib.auth.urls')),
+    # registration page
+    path('register/', views.register, name='register'),
 
 ]
